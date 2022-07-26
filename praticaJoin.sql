f6cfc18a15c90b5463b140b083705782a2c02996
Tutuezin <vinnycamara1@gmail.com>
2022-07-26 19:51:41 -0300

    :sparkles: feat: QUESTAO 2 INC + QUESTAO 3

--- praticaJoin.sql
+++ praticaJoin.sql
@@ -5,3 +5,26 @@ JOIN cities
 ON users."cityId" = cities.id;
 
 /* QUESTAO 2 */
+SELECT u.name, t.message 
+FROM users u
+JOIN testimonials t
+ON u.id = t."writerId"
+JOIN testimonials tr
+ON u.id = tr."recipientId";
+
+
+/* QUESTAO 3 */
+SELECT 
+users.name,
+courses.name AS course,
+schools.name AS school,
+educations."endDate"
+FROM users
+JOIN educations
+ON users.id = 30
+JOIN courses
+ON educations."courseId" = courses.id
+JOIN schools
+ON educations."schoolId" = schools.id
+WHERE educations.status = 'finished';
+
