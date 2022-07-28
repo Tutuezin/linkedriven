9dccbc9b8e21c7e57e9e1a088b64da8fe8175cf8
Tutuezin <vinnycamara1@gmail.com>
2022-07-28 16:05:20 -0300

    :sparkles: feat: QUESTAO 3 / GROUP BY

--- praticaGroupBy.sql
+++ praticaGroupBy.sql
@@ -9,3 +9,8 @@ where status = 'finished'
 group by "userId";
 
 /* QUESTAO 3 */
+SELECT users.name AS name, COUNT(testimonials."writerId") AS "testimonailCount"
+FROM testimonials
+JOIN users ON users.id = testimonials."writerId"
+WHERE testimonials."writerId" = 435
+GROUP BY users.name;
